35ea7836c910cfbe9e09b1fab5820d7f7c2cf794
sarmat <skhachirov12@gmail.com>
2021-10-29 14:05:14 +0300

    fix: modify script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
   const StreetCred = await ethers.getContractFactory("StreetCred");
   const streetCred = await StreetCred.deploy();
 
