7acbabafd533808e18742198199c3be441e6e313
sarmat <skhachirov12@gmail.com>
2021-11-08 09:58:12 +0300

    feat: set ledger as public, format

--- contracts/StreetCred.sol
+++ contracts/StreetCred.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract StreetCred is Ownable {
-  mapping(address => OwnedNFT[]) private ledger;
+  mapping(address => OwnedNFT[]) public ledger;
 
   struct Items {
     address userAddress;
@@ -18,16 +17,18 @@ contract StreetCred is Ownable {
     uint256[] ownedItemIds;
   }
 
-  function addItem(OwnedNFT[] memory _items) public  onlyOwner returns(bool success) {
-    for(uint256 i = 0; i < _items.length; i++) {
-      ledger[_items[i].userAddress].push(OwnedNFT(_items[i].userAddress, _items[i].contractAddress, _items[i].ownedItemIds));
+  function addItem(OwnedNFT[] memory _items) public onlyOwner returns (bool success) {
+    for (uint256 i = 0; i < _items.length; i++) {
+      ledger[_items[i].userAddress].push(
+        OwnedNFT(_items[i].userAddress, _items[i].contractAddress, _items[i].ownedItemIds)
+      );
     }
 
     return true;
   }
 
-  function deleteItem(address[] memory _users) public onlyOwner returns(bool success) {
-    for(uint256 i = 0; i < _users.length; i++) {
+  function deleteItem(address[] memory _users) public onlyOwner returns (bool success) {
+    for (uint256 i = 0; i < _users.length; i++) {
       delete ledger[_users[i]];
     }
 
