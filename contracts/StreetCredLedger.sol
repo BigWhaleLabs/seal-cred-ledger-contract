23129e96173489a734d38e960ab06c108908ee21
Paul Oh <paul.oh1221@gmail.com>
2022-04-18 21:39:17 -0400

    addRoot function in streetcredledger

--- contracts/StreetCredLedger.sol
+++ contracts/StreetCredLedger.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./SCERC721Derivative.sol";
+import "./Verifier.sol";
 
 /**
  * @title Street Cred Ledger
@@ -10,8 +12,31 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract StreetCredLedger is Ownable {
   // State
   mapping(address => bytes32) public ledger;
+  mapping(address => address) public tokenToDerivative;
+  Verifier public verifier;
   // Events
   event SetMerkleRoot(address tokenAddress, bytes32 merkleRoot);
+
+  constructor() {
+    verifier = new Verifier();
+  }
+
+  function addRoot(
+    address tokenAddress,
+    bytes32 merkleRoot,
+    string memory tokenName,
+    string memory tokenSymbol
+  ) external onlyOwner {
+    SCERC721Derivative derivative = new SCERC721Derivative(
+      tokenAddress,
+      address(this),
+      tokenName,
+      tokenSymbol
+    );
+    ledger[tokenAddress] = merkleRoot;
+    tokenToDerivative[tokenAddress] = address(derivative);
+    emit SetMerkleRoot(tokenAddress, merkleRoot);
+  }
 
   /**
    * @dev Sets the Merkle root for a given ERC-721 token
