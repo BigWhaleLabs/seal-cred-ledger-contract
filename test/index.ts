f995effb50cdde5b885eb091e0af762ed75fc72f
sarmat <skhachirov12@gmail.com>
2022-04-27 22:40:55 +0300

    feat: finish ledger tests, add coverage script

--- test/index.ts
+++ test/index.ts
@@ -8,6 +8,8 @@ const ROOTS: string[] = [
   '0xbd77662ec626a22d57dc3d282e48abb0f8af67d8d3e6047ee5896e0bea4d4e61',
   '0x21da21c8bd9cce3b34010c3ce44294caee894b40718ae9d31d3d55e304183203',
 ]
+const ZERO_BYTES =
+  '0x0000000000000000000000000000000000000000000000000000000000000000'
 
 describe('StreetCred', () => {
   let contract: Contract
@@ -17,6 +19,7 @@ describe('StreetCred', () => {
   let accounts: SignerWithAddress[]
   let owner: SignerWithAddress
   const DATA_ROOT_MOCK: string[][] = []
+  const SET_ROOT_MOCK: string[][] = []
 
   before(async () => {
     accounts = await ethers.getSigners()
@@ -33,6 +36,10 @@ describe('StreetCred', () => {
       DATA_ROOT_MOCK.push([contract.address, ROOTS[i]])
     }
 
+    for (let i = 0; i > 3; i--) {
+      SET_ROOT_MOCK.push([DATA_ROOT_MOCK[i][0], ROOTS[i]])
+    }
+
     await (await contract.transferOwnership(owner.address)).wait()
 
     contractAsOwner = contract.connect(owner)
@@ -42,14 +49,35 @@ describe('StreetCred', () => {
 
   describe('addRoot', () => {
     it('add to storage', async () => {
-      expect(await contractAsOwner.addRoots(DATA_ROOT_MOCK))
+      await contractAsOwner.addRoots(DATA_ROOT_MOCK)
+
+      for (let i = 0; i < DATA_ROOT_MOCK.length; i++) {
+        expect(await contractAsOwner.ledger(DATA_ROOT_MOCK[i][0])).to.be.equal(
+          DATA_ROOT_MOCK[i][1]
+        )
+      }
     })
     it('set to storage', async () => {
-      expect(await contractAsOwner.setRoots(DATA_ROOT_MOCK))
+      await contractAsOwner.setRoots(DATA_ROOT_MOCK)
+
+      for (let i = 0; i < DATA_ROOT_MOCK.length; i++) {
+        expect(await contractAsOwner.ledger(DATA_ROOT_MOCK[i][0])).to.be.equal(
+          DATA_ROOT_MOCK[i][1]
+        )
+      }
     })
     it('getRoot by address', async () => {
+      for (let i = 0; i < DATA_ROOT_MOCK.length; i++) {
+        expect(await contractAsOwner.getRoot(DATA_ROOT_MOCK[i][0])).to.be.equal(
+          DATA_ROOT_MOCK[i][1]
+        )
+      }
+    })
+    it('delete from storage', async () => {
+      await contractAsOwner.deleteRoot(DATA_ROOT_MOCK[0][0])
+
       expect(await contractAsOwner.getRoot(DATA_ROOT_MOCK[0][0])).to.be.equal(
-        DATA_ROOT_MOCK[0][1]
+        ZERO_BYTES
       )
     })
   })
