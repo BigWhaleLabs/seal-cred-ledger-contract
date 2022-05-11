2ce45b46683845d11d562131b4e28862df69abf0
sarmat <skhachirov12@gmail.com>
2022-05-11 21:59:51 +0300

    extraction script

---
+++ scripts/extractVerificationInput.ts
@@ -0,0 +1,28 @@
+import fs from 'fs'
+import path from 'path'
+import FindFiles from 'file-regex'
+
+void (async () => {
+  const fileName = await FindFiles(
+    path.join(__dirname, `../artifacts/build-info`),
+    /\/.*?\.json$/g,
+    5
+  )
+  const compiledContract = await import(
+    `../artifacts/build-info/${fileName[0].file}`
+  )
+
+  fs.writeFile(
+    'SCERC721Derivative.json',
+    JSON.stringify(compiledContract.input),
+    'utf8',
+    function (err) {
+      if (err) {
+        console.log('An error occured while writing JSON Object to File.')
+        return console.log(err)
+      }
+
+      console.log('SCERC721Derivative.json file has been saved.')
+    }
+  )
+})()
