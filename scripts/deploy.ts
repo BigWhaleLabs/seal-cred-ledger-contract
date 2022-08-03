61c6b82d7cc6017be877ea305172ba0b8e336b6c
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-08-03 15:49:37 -0700

    add address field

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -122,7 +122,7 @@ async function main() {
     }
 
     // Print out the information
-    console.log(`${contract} deployed and verified on Etherscan!`)
+    console.log(`${contract.address} deployed and verified on Etherscan!`)
     console.log('Contract address:', address)
     console.log(
       'Etherscan URL:',
