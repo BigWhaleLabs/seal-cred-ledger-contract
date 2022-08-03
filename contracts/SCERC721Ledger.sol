e839ce4a7564ce4d1cb9643d19ad0700c39243d5
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-08-03 09:43:33 -0700

    remove library

--- contracts/SCERC721Ledger.sol
+++ contracts/SCERC721Ledger.sol
@@ -61,7 +61,6 @@ pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import "./base/Ledger.sol";
-import "./libraries/ParseAddress.sol";
 import "./SCERC721Derivative.sol";
 
 uint256 constant addressLength = 42;
@@ -71,8 +70,6 @@ uint256 constant addressLength = 42;
  * @dev Creates SCERC721Derivatives, remembers them and proxies mint calls to them
  */
 contract SCERC721Ledger is Ledger {
-  using ParseAddress for string;
-
   // State
   uint256 public immutable network;
 
@@ -144,6 +141,35 @@ contract SCERC721Ledger is Ledger {
       result[i] = bytes1(uint8(input[i]));
     }
     string memory addressString = string(result);
-    return (addressString, addressString.toAddress());
+    return (addressString, _toAddress(addressString));
+  }
+
+  // Credit to: https://github.com/provable-things/ethereum-api
+  function _toAddress(string memory _a) internal pure returns (address) {
+    bytes memory tmp = bytes(_a);
+    uint160 iaddr = 0;
+    uint160 b1;
+    uint160 b2;
+    for (uint256 i = 2; i < 2 + 2 * 20; i += 2) {
+      iaddr *= 256;
+      b1 = uint160(uint8(tmp[i]));
+      b2 = uint160(uint8(tmp[i + 1]));
+      if ((b1 >= 97) && (b1 <= 102)) {
+        b1 -= 87;
+      } else if ((b1 >= 65) && (b1 <= 70)) {
+        b1 -= 55;
+      } else if ((b1 >= 48) && (b1 <= 57)) {
+        b1 -= 48;
+      }
+      if ((b2 >= 97) && (b2 <= 102)) {
+        b2 -= 87;
+      } else if ((b2 >= 65) && (b2 <= 70)) {
+        b2 -= 55;
+      } else if ((b2 >= 48) && (b2 <= 57)) {
+        b2 -= 48;
+      }
+      iaddr += (b1 * 16 + b2);
+    }
+    return address(iaddr);
   }
 }
