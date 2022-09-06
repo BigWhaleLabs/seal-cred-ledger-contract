be59b4071a8c1e77cae6aed55002d4bc4d30af83
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-09-06 09:54:00 -0700

    add farcaster ledger

---
+++ contracts/SCFarcasterDerivative.sol
@@ -0,0 +1,120 @@
+//                                                                        ,-,
+//                            *                      .                   /.(              .
+//                                       \|/                             \ {
+//    .                 _    .  ,   .    -*-       .                      `-`
+//     ,'-.         *  / \_ *  / \_      /|\         *   /\'__        *.                 *
+//    (____".         /    \  /    \,     __      .    _/  /  \  * .               .
+//               .   /\/\  /\/ :' __ \_  /  \       _^/  ^/    `—./\    /\   .
+//   *       _      /    \/  \  _/  \-‘\/  ` \ /\  /.' ^_   \_   .’\\  /_/\           ,'-.
+//          /_\   /\  .-   `. \/     \ /.     /  \ ;.  _/ \ -. `_/   \/.   \   _     (____".    *
+//     .   /   \ /  `-.__ ^   / .-'.--\      -    \/  _ `--./ .-'  `-/.     \ / \             .
+//        /     /.       `.  / /       `.   /   `  .-'      '-._ `._         /.  \
+// ~._,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'
+// ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~~
+// ~~    ~~~~    ~~~~     ~~~~   ~~~~    ~~~~    ~~~~    ~~~~    ~~~~    ~~~~    ~~~~    ~~~~    ~~
+//     ~~     ~~      ~~      ~~      ~~      ~~      ~~      ~~       ~~     ~~      ~~      ~~
+//                          ๐
+//                                                                              _
+//                                                  ₒ                         ><_>
+//                                  _______     __      _______
+//          .-'                    |   _  "\   |" \    /" _   "|                               ๐
+//     '--./ /     _.---.          (. |_)  :)  ||  |  (: ( \___)
+//     '-,  (__..-`       \        |:     \/   |:  |   \/ \
+//        \          .     |       (|  _  \\   |.  |   //  \ ___
+//         `,.__.   ,__.--/        |: |_)  :)  |\  |   (:   _(  _|
+//           '._/_.'___.-`         (_______/   |__\|    \_______)                 ๐
+//
+//                  __   __  ___   __    __         __       ___         _______
+//                 |"  |/  \|  "| /" |  | "\       /""\     |"  |       /"     "|
+//      ๐          |'  /    \:  |(:  (__)  :)     /    \    ||  |      (: ______)
+//                 |: /'        | \/      \/     /' /\  \   |:  |   ₒ   \/    |
+//                  \//  /\'    | //  __  \\    //  __'  \   \  |___    // ___)_
+//                  /   /  \\   |(:  (  )  :)  /   /  \\  \ ( \_|:  \  (:      "|
+//                 |___/    \___| \__|  |__/  (___/    \___) \_______)  \_______)
+//                                                                                     ₒ৹
+//                          ___             __       _______     ________
+//         _               |"  |     ₒ     /""\     |   _  "\   /"       )
+//       ><_>              ||  |          /    \    (. |_)  :) (:   \___/
+//                         |:  |         /' /\  \   |:     \/   \___  \
+//                          \  |___     //  __'  \  (|  _  \\    __/  \\          \_____)\_____
+//                         ( \_|:  \   /   /  \\  \ |: |_)  :)  /" \   :)         /--v____ __`<
+//                          \_______) (___/    \___)(_______/  (_______/                  )/
+//                                                                                        '
+//
+//            ๐                          .    '    ,                                           ₒ
+//                         ₒ               _______
+//                                 ____  .`_|___|_`.  ____
+//                                        \ \   / /                        ₒ৹
+//                                          \ ' /                         ๐
+//   ₒ                                        \/
+//                                   ₒ     /      \       )                                 (
+//           (   ₒ৹               (                      (                                  )
+//            )                   )               _      )                )                (
+//           (        )          (       (      ><_>    (       (        (                  )
+//     )      )      (     (      )       )              )       )        )         )      (
+//    (      (        )     )    (       (              (       (        (         (        )
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "./base/Derivative.sol";
+import "./interfaces/IFarcasterCheckerVerifier.sol";
+import "./models/FarcasterProof.sol";
+
+contract SCFarcasterDerivative is Derivative {
+  constructor(
+    address _ledgerContract,
+    address _verifierContract,
+    uint256 _attestorPublicKey
+  )
+    Derivative(
+      _ledgerContract,
+      _verifierContract,
+      _attestorPublicKey,
+      "Farcaster (derivative)",
+      "FARCASTER-d"
+    )
+  {}
+
+  function mint(FarcasterProof memory proof) external {
+    _mint(msg.sender, proof);
+  }
+
+  function mintWithSender(address sender, FarcasterProof memory proof)
+    external
+    onlyOwner
+  {
+    _mint(sender, proof);
+  }
+
+  function _mint(address sender, FarcasterProof memory proof) internal {
+    _checkAttestor(proof.input[10]);
+    _checkFarcasterWord(proof);
+    _checkProof(proof);
+    _mintWithNullifier(sender, proof.input[9]);
+  }
+
+  function _checkFarcasterWord(FarcasterProof memory proof) internal pure {
+    bytes memory farcasterBytes = bytes("farcaster");
+    for (uint8 i = 0; i < 9; i++) {
+      require(
+        uint8(proof.input[i]) == uint8(farcasterBytes[i]),
+        "This ZK proof is not from the farcaster"
+      );
+    }
+  }
+
+  function _checkProof(FarcasterProof memory proof) internal view {
+    require(
+      IFarcasterCheckerVerifier(verifierContract).verifyProof(
+        proof.a,
+        proof.b,
+        proof.c,
+        proof.input
+      ),
+      "Invalid ZK proof"
+    );
+  }
+}
---
+++ contracts/SCFarcasterLedger.sol
@@ -0,0 +1,103 @@
+//                                                                        ,-,
+//                            *                      .                   /.(              .
+//                                       \|/                             \ {
+//    .                 _    .  ,   .    -*-       .                      `-`
+//     ,'-.         *  / \_ *  / \_      /|\         *   /\'__        *.                 *
+//    (____".         /    \  /    \,     __      .    _/  /  \  * .               .
+//               .   /\/\  /\/ :' __ \_  /  \       _^/  ^/    `—./\    /\   .
+//   *       _      /    \/  \  _/  \-‘\/  ` \ /\  /.' ^_   \_   .’\\  /_/\           ,'-.
+//          /_\   /\  .-   `. \/     \ /.     /  \ ;.  _/ \ -. `_/   \/.   \   _     (____".    *
+//     .   /   \ /  `-.__ ^   / .-'.--\      -    \/  _ `--./ .-'  `-/.     \ / \             .
+//        /     /.       `.  / /       `.   /   `  .-'      '-._ `._         /.  \
+// ~._,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'
+// ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~~
+// ~~    ~~~~    ~~~~     ~~~~   ~~~~    ~~~~    ~~~~    ~~~~    ~~~~    ~~~~    ~~~~    ~~~~    ~~
+//     ~~     ~~      ~~      ~~      ~~      ~~      ~~      ~~       ~~     ~~      ~~      ~~
+//                          ๐
+//                                                                              _
+//                                                  ₒ                         ><_>
+//                                  _______     __      _______
+//          .-'                    |   _  "\   |" \    /" _   "|                               ๐
+//     '--./ /     _.---.          (. |_)  :)  ||  |  (: ( \___)
+//     '-,  (__..-`       \        |:     \/   |:  |   \/ \
+//        \          .     |       (|  _  \\   |.  |   //  \ ___
+//         `,.__.   ,__.--/        |: |_)  :)  |\  |   (:   _(  _|
+//           '._/_.'___.-`         (_______/   |__\|    \_______)                 ๐
+//
+//                  __   __  ___   __    __         __       ___         _______
+//                 |"  |/  \|  "| /" |  | "\       /""\     |"  |       /"     "|
+//      ๐          |'  /    \:  |(:  (__)  :)     /    \    ||  |      (: ______)
+//                 |: /'        | \/      \/     /' /\  \   |:  |   ₒ   \/    |
+//                  \//  /\'    | //  __  \\    //  __'  \   \  |___    // ___)_
+//                  /   /  \\   |(:  (  )  :)  /   /  \\  \ ( \_|:  \  (:      "|
+//                 |___/    \___| \__|  |__/  (___/    \___) \_______)  \_______)
+//                                                                                     ₒ৹
+//                          ___             __       _______     ________
+//         _               |"  |     ₒ     /""\     |   _  "\   /"       )
+//       ><_>              ||  |          /    \    (. |_)  :) (:   \___/
+//                         |:  |         /' /\  \   |:     \/   \___  \
+//                          \  |___     //  __'  \  (|  _  \\    __/  \\          \_____)\_____
+//                         ( \_|:  \   /   /  \\  \ |: |_)  :)  /" \   :)         /--v____ __`<
+//                          \_______) (___/    \___)(_______/  (_______/                  )/
+//                                                                                        '
+//
+//            ๐                          .    '    ,                                           ₒ
+//                         ₒ               _______
+//                                 ____  .`_|___|_`.  ____
+//                                        \ \   / /                        ₒ৹
+//                                          \ ' /                         ๐
+//   ₒ                                        \/
+//                                   ₒ     /      \       )                                 (
+//           (   ₒ৹               (                      (                                  )
+//            )                   )               _      )                )                (
+//           (        )          (       (      ><_>    (       (        (                  )
+//     )      )      (     (      )       )              )       )        )         )      (
+//    (      (        )     )    (       (              (       (        (         (        )
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "./base/Ledger.sol";
+import "./SCFarcasterDerivative.sol";
+
+/**
+ * @title SealCred Farcaster Ledger
+ * @dev Creates SCFarcasterDerivatives, remembers them and proxies mint calls to them
+ */
+contract SCFarcasterLedger is Ledger {
+  constructor(
+    address _verifierContract,
+    uint256 _attestorPublicKey,
+    address _forwarder
+  ) Ledger(_verifierContract, _attestorPublicKey, _forwarder) {}
+
+  /**
+   * @dev Universal mint function that proxies mint call to derivatives and creates derivatives if necessary
+   */
+  function mint(FarcasterProof memory proof) external virtual {
+    string memory original = "farcaster";
+    // Check if derivative already exists
+    if (!_checkDerivativeExistence(original)) {
+      _spawnDerivative();
+    }
+    // Proxy mint call
+    SCFarcasterDerivative(originalToDerivative[original]).mintWithSender(
+      _msgSender(),
+      proof
+    );
+  }
+
+  /**
+   * @dev Create a new derivative
+   */
+  function _spawnDerivative() internal {
+    SCFarcasterDerivative derivative = new SCFarcasterDerivative(
+      address(this),
+      verifierContract,
+      attestorPublicKey
+    );
+    _registerDerivative("Farcaster", address(derivative));
+  }
+}
